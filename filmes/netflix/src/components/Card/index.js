b16159b50391cb7d6ba032e585b43447c52ca79c
Leonardo Furmann <leohitnerf@gmail.com>
2023-04-23 19:43:05 -0300

    select

--- filmes/netflix/src/components/Card/index.js
+++ filmes/netflix/src/components/Card/index.js
@@ -16,6 +16,8 @@ export default function Card() {
 
     const [filmes, setFilmes] = useState([]);
     const [busca, setBusca] = useState('');
+    const [filtro, setFiltro] = useState('');
+    const [filmesBuscados, setfilmesBuscados] = useState([]);
 
     const options = {
         method: 'GET'
@@ -25,24 +27,54 @@ export default function Card() {
         fetch('https://my-json-server.typicode.com/marycamila184/movies/movies')
             .then(response => response.json())
             .then(data => setFilmes(data))
-            .catch(err => console.error(err))
+            .then(data => setfilmesBuscados)
+            .catch(err => console.error(err));
     }, []);
 
-        
-       const filmesBuscados = useMemo(() => {
-       const lowerBusca = busca.toLowerCase();
-            return filmes.filter((filme) => filme.titulo.toLowerCase().includes(lowerBusca));
-       },[busca]);          
+     useEffect(() => {
+        const lowerBusca = busca.toLowerCase();
+         const filmesBuscados = filmes.filter((filme) => {
+       return filme.titulo.toLowerCase().includes(lowerBusca);
+        });
+        setfilmesBuscados(filmesBuscados);
+    }, [busca, filmes]);
+
+    useEffect(() => {
+        const filmesFiltrados = [...filmesBuscados].sort((a,b) => {
+            if(filtro === "1"){
+                return a.titulo.localeCompare(b.titulo);
+            } 
+            if(filtro === "2"){
+                return b.ano - a.ano;
+            }
+            if(filtro === "3"){
+                return b.nota - a.nota;
+            }
+        });
+        setfilmesBuscados(filmesFiltrados);
+    }, [filtro]);
+
 
     return (
 
         <div className="container text-center">
-
-            <Titulo/>
-
+            <Titulo />  
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Pesquisar Filmes" aria-label="Pesquisar Filmes" value ={busca} onChange={(ev) => setBusca(ev.target.value)}/>
+                
+                <input type="text" class="form-control" placeholder="Pesquisar Filmes" aria-label="Pesquisar Filmes" value={busca} onChange={(ev) => setBusca(ev.target.value)} />
+
+                {/* <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button">Pesquisar</button>
+                </div> */}
+        
+                <select class="custom-select" id="inputGroupSelect02" onChange={(ev) => setFiltro(ev.target.value)}>  
+                    <option selected>Filtrar Filmes</option>
+                    <option value="1">Titulo</option>
+                    <option value="2">Ano</option>
+                    <option value="3">Nota</option>
+                </select>
             </div>
+
 
             <div class="row">
                 {filmesBuscados.map((filme, i) => (
@@ -68,7 +100,7 @@ export default function Card() {
                 ))}
             </div>
 
-        </div>
+        </div >
     );
 }
 
