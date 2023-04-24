a3dd7da342f1c249e6b2b0c7bb3068490c7071b8
Leonardo Furmann <leohitnerf@gmail.com>
2023-04-24 10:18:20 -0300

    comentarios

--- filmes/netflix/src/Pages/detalhes.js
+++ filmes/netflix/src/Pages/detalhes.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Comments from './../components/Comments/index';
 
 function Detalhes() {
     const [filme, setFilme] = useState([]);
-    const {id} = useParams();
-     
+    const { id } = useParams();
+
     const options = {
         method: 'GET'
     };
@@ -19,34 +20,23 @@ function Detalhes() {
 
     return (
         <div class="container">
-            {(() => {
-                    return (
 
-                        <><div className="row">
-                            <><div className="col">
-                                <div className="card" style={{ maxWidth: "500px" }}>
-                                    <img src={filme.poster} alt={filme.titulo} className="card-img-top" />
-                                </div>
-                            </div><div className="col">
-                                    <div className="card mx-auto p-3">
+            <div className="row">
+                <><div className="col">
+                    <div className="card" style={{ maxWidth: "500px" }}>
+                        <img src={filme.poster} alt={filme.titulo} className="card-img-top" />
+                    </div>
+                </div><div className="col">
+                        <div className="card mx-auto p-3">
 
-                                        <h1 className='text-center'>{filme.titulo}</h1>
-                                        <p className='text-center'>{filme.ano}</p>
-                                        <p className='text-center'>{filme.sinopse}</p>
-                                    </div>
-                                </div></>
+                            <h1 className='text-center'>{filme.titulo}</h1>
+                            <p className='text-center'>{filme.ano}</p>
+                            <p className='text-center'>{filme.sinopse}</p>
                         </div>
+                    </div></>
 
-
-                        <div class="input-group mx-auto p-3">
-                                <span class="input-group-text">Comentário</span>
-                                <textarea class="form-control" aria-label="With textarea"></textarea>
-                                <button type="button" class="btn btn-primary">Adicionar</button>
-                        </div></>
-                    )
-               
-            })()}
-
+                    <Comments filme={id}/>
+            </div>
         </div>
     )
 }
